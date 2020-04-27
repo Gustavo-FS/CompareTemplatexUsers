f6bbfef9c7f950480a24c57205b10a7bbf1d08d4
Gustavo-FS <santos.gustavof@gmail.com>
2020-04-27 01:49:16 -0300

    Versão 2.1
    Validação Recursiva entre Template x Usuarios e Usuarios x Templates

--- ValidaOUSCript.ps1
+++ ValidaOUSCript.ps1
@@ -1,66 +1,57 @@
-Start-Transcript -Path .\log.txt
+Import-Module activedirectory 
+$date = Get-Date -format "dd-MM-yy"
+$Server = "SERVER"
+$dir = "DIRETORY"
+cd $dir
+Clear
+Start-Transcript -Path "$dir\logs\log$date.txt"
 
-Write-Host "SCRIPT DE VALIDAÇÂO DE OU x Template - 1.0.0" -ForegroundColor Yellow
+Write-Host "SCRIPT DE VALIDAÃ‡Ã‚O DE OU x Template - 2.1.0" -ForegroundColor Yellow
 Write-Host "Desenvolvido por Gustavo Fernandes" -ForegroundColor Green
+$count = (Get-ADUser -Filter "enabled -eq'true'" -Server $Server).count 
+Write-Host "Atualmente o AD contem $count Usuarios Ativos no Dominio $Server" -ForegroundColor Green
 Date
-
-## Validar se o Arquivo Lista de Templates Existe 
-
+###########################################################  
 if (test-path -path .\templates.txt) {
-
-## Especificar Servidores
-
-$Server = "XXXX"
-$Server2 = "YYYYY"
-
-## Obter Lista de Tempaltes 
-
-foreach( $Template in Get-Content .\templates.txt) {
-    $userCN = Get-ADUser $Template -Properties CanonicalName -Server $Server
+###########################################################  
+foreach( $SID in Get-Content .\templates.txt) { 
+    $userCN = Get-ADUser $SID -Properties CanonicalName -Server $Server
     $userOU = ($userCN.DistinguishedName -split ",",2)[1]
-    $users = Get-ADUser -Filter * -SearchBase $UserOU -Server $Server	
-	$user = get-aduser $Template -Properties memberof -Server $Server 
-	$groups = Foreach ($group in $user.memberof) {
-		Try {
-		(Get-ADGroup "$group" -Server $Server).Name
-        $members = Get-ADGroupMember -Identity $group -Recursive -Server $Server | Select -ExpandProperty Name -ErrorAction Stop
-		}Catch {
-## Use Para Grupos em mais de um Dominio 		
-        (Get-ADGroup "$group" -Server $Server2).Name
-        $members = Get-ADGroupMember -Identity $group -Recursive -Server $Server2 | Select -ExpandProperty Name
-		}
-        $users | ForEach-Object {
-        $user = $_.Name
-        If ($members -notcontains  $user) {
-        Write-Host  "O Usuário " -ForegroundColor White -NoNewline; 
-		Write-Host  "$user " -ForegroundColor Green -NoNewline;
-		Write-Host  "não pertence ao grupo " -ForegroundColor White -NoNewline;
-		Write-Host  "$group " -ForegroundColor Magenta -NoNewline;		
-		Write-Host  "pertence aos Grupos" -ForegroundColor White -NoNewline;
-		$groupsOK = Get-ADUser -Filter {name -like $user} -Properties MemberOf -Server $Server| Select -ExpandProperty MemberOf	
-		Write-Host  "$groupsOK " -ForegroundColor Magenta -NoNewline;
-		Write-Host  "na Unidade Organizacional " -ForegroundColor White -NoNewline;
-        Write-Host  "$UserOU " -ForegroundColor Yellow;
-		Write-Host   " ----"
-		
-## Exporta resultado Excel 	
-		
-		$Result = @{
-		"O Usuaario" = $user
-		"Nao Pertence aos Grupos" = $group
-		"Pertence aos Grupos" = [string]$groupsOK
-		"na Unidade Organizacional" = $UserOU	
-		}		
-		$ExportCSV = New-Object -TypeName psobject -Property $Result                
-		$ExportCSV | Select-Object 'O Usuario','Nao Pertence aos Grupos','Pertence aos Grupos','na Unidade Organizacional' | Export-Csv .\Resultado.csv -NoTypeInformation -Append
+    $users = Get-ADUser -Filter * -SearchBase $UserOU -Server $Server
+    Foreach ($DID in $users.SamAccountName) {
+##################################################################
+$SIDUPN = Get-ADUser -Filter {Samaccountname -eq $SID} -Server $Server 
+$SIDDC0 = $SIDUPN.UserPrincipalName 
+$SIDDC = $SIDDC0.split("@")[1] 
+###########################################################  
+$DIDUPN = Get-ADUser -Filter {Samaccountname -eq $DID} -Server $Server
+$DIDDC0 = $DIDUPN.UserPrincipalName 
+$DIDDC = $DIDDC0.split("@")[1] 
+###########################################################  
+$SGrps = Get-ADPrincipalGroupMembership $SID  -Server $Server| select -ExpandProperty name 
+$DGrps = Get-ADPrincipalGroupMembership $DID  -Server $Server| select -ExpandProperty name 
+$diff = Compare-Object -ReferenceObject $SGrps -DifferenceObject  $DGrps | Where-Object {$_.SideIndicator -eq "<="}  | select -ExpandProperty InputObject  
+If($diff -ne $null){
+Write-Host "";
+Write-Host "-----------------------------------------------------------" 
+Write-Host "Grupo pertence a ($SID) mas nÃ£o pertence a ($DID). " -ForegroundColor YELLOW
+$diff 
+Write-Host"";
 }
+$diff2 = Compare-Object -ReferenceObject $SGrps -DifferenceObject  $DGrps | Where-Object {$_.SideIndicator -eq "=>"}  | select -ExpandProperty InputObject  
+If($diff -ne $null){
+Write-Host "";
+Write-Host "-----------------------------------------------------------" 
+Write-Host "Grupo pertence a  ($DID) mas nÃ£o pertence a ($SID). " -ForegroundColor RED 
+$diff2 
+Write-Host "";
 }
 }
 }
 } else {
-Write-Host "Erro - Lista não encontrada - Adicone o arquivo template.txt com a lista de templates a ser validada"
+Write-Host "Erro - Lista nÃ£o encontrada - Adicione o arquivo template.txt com a lista de templates a ser validada"
 }
-
+###########################################################  
 Write-Host "FIM DO SCRIPT - um arquivo de log foi gerado"
-Stop-Transcript
+
 $Close = read-host "Pressione Enter para Encerrar"
